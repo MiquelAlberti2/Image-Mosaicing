00799249d0327d6aae79c6495ae8b7a36038b370
MiquelAlberti2 <miquel.alberti2@gmail.com>
2023-03-14 20:49:11 -0400

    fancy plot of correspondances

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import imageio.v3 as iio # to read and write images
 import matplotlib.pyplot as plt
-
+from skimage import draw
 
 # Other files in the project
 from harris_corner_detector import detect_features
@@ -9,14 +9,29 @@ from NCC import find_correspondances
 from homography_RANSAC import estimate_homography
 from merge_images import warp_image1_onto_image2
 
-
-
 def rgb_to_gray(rgb):
 
     # compute a weighted average of RGB colors to obtain a greyscale value
     # weights correspond to the luminosity of each color channel
     # we also normalize the image
     return (1/255)*np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
+
+def plot_correspondaces(img1, img2, corresp):
+    img3 = np.zeros((img1.shape[0]+img2.shape[0], max(img1.shape[1], img2.shape[1])))
+    img3[:img1.shape[0], :img1.shape[1]] = img1
+    img3[img1.shape[0]:, :img2.shape[1]] = img2
+
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax.imshow(img3, cmap='gray')
+
+    for match in corresp:
+        pt1 = match[0]
+        pt2 = match[1]
+        r, c = draw.line(pt1[0], pt1[1], pt2[0] + img1.shape[0], pt2[1])
+        ax.plot(c, r, linewidth=0.4, color='blue')
+
+    plt.show()
+
 
 ##################
 # Read the two images
@@ -54,28 +69,9 @@ plt.show()
 # get a list containing all the feature correspondances
 corresp = find_correspondances(grey_img1, grey_img2, features1, features2)
 
-print(corresp)
-
-
-
-# DRAW THE CORRESPONDENCES BETWEEN TWO IMAGES
-
-from skimage import draw
-
-img3 = np.zeros((grey_img1.shape[0]+grey_img2.shape[0], max(grey_img1.shape[1], grey_img2.shape[1])))
-img3[:grey_img1.shape[0], :grey_img1.shape[1]] = grey_img1
-img3[grey_img1.shape[0]:, :grey_img2.shape[1]] = grey_img2
-
-fig, ax = plt.subplots(figsize=(10,10))
-ax.imshow(img3, cmap='gray')
-
-for match in corresp:
-    pt1 = match[0]
-    pt2 = match[1]
-    r, c = draw.line(pt1[0], pt1[1], pt2[0] + grey_img1.shape[0], pt2[1])
-    ax.plot(c, r, linewidth=0.4, color='blue')
-
-plt.show()
+print("INITIAL CORRESPONDACES: ", corresp)
+# plot the correspondaces between the two images
+plot_correspondaces(grey_img1, grey_img2, corresp)
 
 
 ##################
@@ -83,7 +79,12 @@ plt.show()
 ##################
 
 # estimate the homography matrix from the computed correspondances
-homography = estimate_homography(corresp)
+homography, inliers = estimate_homography(corresp)
+
+print("INLIERS CORRESPONDACES: ", inliers)
+# plot the correspondaces between the two images
+plot_correspondaces(grey_img1, grey_img2, inliers)
+
 
 ##################
 # Warp one image onto the other to obtain the final mosaic
